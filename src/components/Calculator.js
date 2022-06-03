87bc3ab05536399487178c9bb5e175e260e647e4
myelin0 <emi.veisllari@gmail.com>
2022-06-03 23:28:25 +0200

    Add Router

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -7,48 +7,50 @@ const Calculator = () => {
     next: null,
     operation: null,
   });
-
   const { total, next, operation } = state;
 
   const clickHandler = (e) => {
     const data = calculate(state, e.target.textContent);
     setState(data);
   };
-
   return (
-    <div className="container">
-      <div className="calc-grid">
-        <div className="output">
-          {`${total || ''} ${operation || ''} ${next || ''}`}
-        </div>
-        <div
-          className="input"
-          aria-hidden
-          onClick={clickHandler}
-          onKeyDown={clickHandler}
-        >
-          <button type="button">AC</button>
-          <button type="button">+/-</button>
-          <button type="button">%</button>
-          <button type="button" className="orange">÷</button>
-          <button type="button">7</button>
-          <button type="button">8</button>
-          <button type="button">9</button>
-          <button type="button" className="orange">x</button>
-          <button type="button">4</button>
-          <button type="button">5</button>
-          <button type="button">6</button>
-          <button type="button" className="orange">-</button>
-          <button type="button">1</button>
-          <button type="button">2</button>
-          <button type="button">3</button>
-          <button type="button" className="orange">+</button>
-          <button type="button" className="span-two">0</button>
-          <button type="button">.</button>
-          <button type="button" className="orange">=</button>
+    <>
+      <div className="calcWrapper">
+        <h2>Let`s do some math!</h2>
+        <div className="container">
+          <div className="calc-grid">
+            <div className="output" data-testid="screen-output">
+              {`${total || ''} ${operation || ''} ${next || ''}`}
+            </div>
+            <div
+              className="input"
+              aria-hidden
+              onClick={clickHandler}
+            >
+              <button type="button">AC</button>
+              <button type="button">+/-</button>
+              <button type="button">%</button>
+              <button type="button" className="orange">÷</button>
+              <button type="button">7</button>
+              <button type="button">8</button>
+              <button type="button">9</button>
+              <button type="button" className="orange">x</button>
+              <button type="button">4</button>
+              <button type="button">5</button>
+              <button type="button">6</button>
+              <button type="button" className="orange">-</button>
+              <button type="button">1</button>
+              <button type="button">2</button>
+              <button type="button">3</button>
+              <button type="button" className="orange">+</button>
+              <button type="button" className="span-two">0</button>
+              <button type="button">.</button>
+              <button type="button" className="orange">=</button>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
